19b4b48854b359a924d3bbfe5d8a22196c857b9e
Catherine Salazar <catcreates13@gmail.com>
2018-09-26 00:24:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,11 +23,11 @@ class Application
         @@cart.each do |c|
           resp.write "#{c}\n"
       end
-  #   end
-  #
-  # elsif req.path.match(/add/)
-  #   if @@items.include?(item)
-  #     @@cart << item
+    end
+
+  elsif req.path.match(/add/)
+    if @@items.include?(item)
+      @@cart << item
       else
         resp.write "We don't have that item"
     end
